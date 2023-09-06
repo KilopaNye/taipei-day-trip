6170c63e4831fef4dae971d08e962641f5228a18
Kilopa Nye <steve69988@gmail.com>
2023-09-06 11:06:24 +0800

    第二周作業反饋修正

--- public/js/index.js
+++ public/js/index.js
@@ -11,6 +11,7 @@ function leftScroll() {
 };
 
 let page = 0;
+console.log(page+"wwwwwwwwwwwwwwwwwwwwwwwwww")
 
 fetch("/api/attractions").then(response => response.json()).then(data => {
     console.log("讀取成功", data);
@@ -79,63 +80,67 @@ fetch("/api/mrts").then(response => response.json()).then(mrtdatas => {
     console.error("發生錯誤", error);
 });
 
+
+let keyword = "";
 let loadMore = function () {
-    let pageNum = page;
-    console.log(page);
-    if (page != null)
-        fetch(`/api/attractions?page=${pageNum}`).then(response => response.json()).then(data => {
+    pageGet = page;
+    if (pageGet != null) {
+        let pageNum = pageGet;
+        fetch(`/api/attractions?page=${pageNum}&keyword=${keyword}`).then(response => response.json()).then(data => {
+            console.log(keyword)
             console.log("讀取成功", data);
-            page = data["nextPage"];
             setNum = page * 12;
+            page = data["nextPage"];
             dataLength = data["data"].length;
-            if (page != null)
-                for (let x = 0; x < dataLength; x++) {
-                    i = x + setNum;
-                    //選擇父層
-                    let firstFa = document.querySelector(".images-box");
-                    //創建attraction
-                    let firstDiv = document.createElement("div");
-                    firstDiv.classList.add("attraction", "box" + i);
-                    firstFa.appendChild(firstDiv);
-                    //創建image
-                    let image = document.querySelector(".box" + i);
-                    let imageDiv = document.createElement("img");
-                    imageDiv.classList.add("image", "imagesNum" + i)
-                    image.appendChild(imageDiv);
-                    //創建attraction-img
-                    let attractionImgDiv = document.createElement("img");
-                    attractionImgDiv.classList.add("attraction-img");
-                    image.appendChild(attractionImgDiv);
-                    attractionImgDiv.src = data["data"][x]["images"][0];
-                    image.appendChild(attractionImgDiv);
-                    //創建opacity
-                    let opacityDiv = document.createElement("div");
-                    opacityDiv.classList.add("opacity");
-                    image.appendChild(opacityDiv);
-                    //創建attraction-name
-                    let attractionNameDiv = document.createElement("div");
-                    attractionNameDiv.classList.add("attraction-name");
-                    attractionNameDiv.textContent = data["data"][x]["name"];
-                    image.appendChild(attractionNameDiv);
-                    //創建img-bottom
-                    let imgBottom = document.querySelector(".box" + i);
-                    let imgBottomDiv = document.createElement("div");
-                    imgBottomDiv.classList.add("img-bottom");
-                    imgBottom.appendChild(imgBottomDiv);
-                    //創建mrt-name
-                    let mrtNameDiv = document.createElement("div");
-                    mrtNameDiv.classList.add("mrt-name")
-                    mrtNameDiv.textContent = data["data"][x]["mrt"]
-                    imgBottom.appendChild(mrtNameDiv);
-                    //創建view-class
-                    let viewClassDiv = document.createElement("div");
-                    viewClassDiv.classList.add("view-class");
-                    viewClassDiv.textContent = data["data"][x]["category"]
-                    imgBottom.appendChild(viewClassDiv);
-                }
+            for (let x = 0; x < dataLength; x++) {
+                i = x + setNum;
+                //選擇父層
+                let firstFa = document.querySelector(".images-box");
+                //創建attraction
+                let firstDiv = document.createElement("div");
+                firstDiv.classList.add("attraction", "box" + i);
+                firstFa.appendChild(firstDiv);
+                //創建image
+                let image = document.querySelector(".box" + i);
+                let imageDiv = document.createElement("img");
+                imageDiv.classList.add("image", "imagesNum" + i)
+                image.appendChild(imageDiv);
+                //創建attraction-img
+                let attractionImgDiv = document.createElement("img");
+                attractionImgDiv.classList.add("attraction-img");
+                image.appendChild(attractionImgDiv);
+                attractionImgDiv.src = data["data"][x]["images"][0];
+                image.appendChild(attractionImgDiv);
+                //創建opacity
+                let opacityDiv = document.createElement("div");
+                opacityDiv.classList.add("opacity");
+                image.appendChild(opacityDiv);
+                //創建attraction-name
+                let attractionNameDiv = document.createElement("div");
+                attractionNameDiv.classList.add("attraction-name");
+                attractionNameDiv.textContent = data["data"][x]["name"];
+                image.appendChild(attractionNameDiv);
+                //創建img-bottom
+                let imgBottom = document.querySelector(".box" + i);
+                let imgBottomDiv = document.createElement("div");
+                imgBottomDiv.classList.add("img-bottom");
+                imgBottom.appendChild(imgBottomDiv);
+                //創建mrt-name
+                let mrtNameDiv = document.createElement("div");
+                mrtNameDiv.classList.add("mrt-name")
+                mrtNameDiv.textContent = data["data"][x]["mrt"]
+                imgBottom.appendChild(mrtNameDiv);
+                //創建view-class
+                let viewClassDiv = document.createElement("div");
+                viewClassDiv.classList.add("view-class");
+                viewClassDiv.textContent = data["data"][x]["category"]
+                imgBottom.appendChild(viewClassDiv);
+            }
         }).catch(error => {
             console.error("發生錯誤", error);
         });
+    }
+
     else {
         return;
     };
@@ -155,9 +160,12 @@ let next = function () {
 //捷運站按鈕
 function keywordSearch(key) {
     keyword = key.textContent;
+    console.log(keyword)
+    let placeholder = document.querySelector(".search")
+    placeholder.value = keyword;
     fetch(`/api/attractions?keyword=${keyword}`).then(response => response.json()).then(data => {
         console.log("讀取成功", data);
-        page = data["nextPage"];
+        mrtPage = data["nextPage"];
         setNum = page * 12;
         dataLength = data["data"].length;
         let photoBox = document.querySelector(".images-box");
@@ -212,14 +220,17 @@ function keywordSearch(key) {
 };
 
 //搜尋欄
+let nextPage = 0;
 function keywords() {
     let keywords = document.querySelector(".search").value;
-    fetch(`/api/attractions?keyword=${keywords}`).then(response => response.json()).then(data => {
-        console.log("讀取成功", data);
-        if (data["data"] != "null") {
-            page = data["nextPage"];
-            setNum = page * 12;
-            dataLength = data["data"].length;
+    keyword = keywords
+    console.log(keywords)
+    fetch(`/api/attractions?keyword=${keywords}`).then(response => response.json()).then(searchData => {
+        console.log("讀取成功", searchData);
+        if (searchData["data"] != "null") {
+            setNum = nextPage * 12;
+            nextPage,page = searchData["nextPage"];
+            dataLength = searchData["data"].length;
             let photoBox = document.querySelector(".images-box");
             photoBox.innerHTML = "";
             for (let x = 0; x < dataLength; x++) {
@@ -239,7 +250,7 @@ function keywords() {
                 let attractionImgDiv = document.createElement("img");
                 attractionImgDiv.classList.add("attraction-img");
                 image.appendChild(attractionImgDiv);
-                attractionImgDiv.src = data["data"][x]["images"][0];
+                attractionImgDiv.src = searchData["data"][x]["images"][0];
                 image.appendChild(attractionImgDiv);
                 //創建opacity
                 let opacityDiv = document.createElement("div");
@@ -248,7 +259,7 @@ function keywords() {
                 //創建attraction-name
                 let attractionNameDiv = document.createElement("div");
                 attractionNameDiv.classList.add("attraction-name");
-                attractionNameDiv.textContent = data["data"][x]["name"];
+                attractionNameDiv.textContent = searchData["data"][x]["name"];
                 image.appendChild(attractionNameDiv);
                 //創建img-bottom
                 let imgBottom = document.querySelector(".box" + i);
@@ -258,12 +269,12 @@ function keywords() {
                 //創建mrt-name
                 let mrtNameDiv = document.createElement("div");
                 mrtNameDiv.classList.add("mrt-name")
-                mrtNameDiv.textContent = data["data"][x]["mrt"]
+                mrtNameDiv.textContent = searchData["data"][x]["mrt"]
                 imgBottom.appendChild(mrtNameDiv);
                 //創建view-class
                 let viewClassDiv = document.createElement("div");
                 viewClassDiv.classList.add("view-class");
-                viewClassDiv.textContent = data["data"][x]["category"]
+                viewClassDiv.textContent = searchData["data"][x]["category"]
                 imgBottom.appendChild(viewClassDiv);
             }
         } else {
